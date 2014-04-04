0e2b0858b6eb790214b5c65db6dcf1bb975754c7
Ryan <rsul.dev@me.com>
2014-04-04 15:03:51 -0400

    Initial Commit

---
+++ HFR/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HFR
+//
+//  Created by Ryan Sullivan on 4/4/14.
+//  Copyright (c) 2014 RSully. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
