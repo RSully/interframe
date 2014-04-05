9dc535e4e8d100c004686ae3a465fca8a9124de6
Ryan <rsul.dev@me.com>
2014-04-05 10:09:20 -0400

    Add initial test code

--- HFR/main.m
+++ HFR/main.m
@@ -7,14 +7,46 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSError *err = nil;
+
+        NSURL *input = [NSURL URLWithString:@""];
+//        NSURL *output = [NSURL URLWithString:@""];
+
+        AVURLAsset *inputAsset = [AVURLAsset URLAssetWithURL:input options:@{}];
+
+        AVAssetReader *inputReader = [[AVAssetReader alloc] initWithAsset:inputAsset error:&err];
+        NSDictionary *videoSettings = @{
+            (NSString*)kCVPixelBufferPixelFormatTypeKey: [NSNumber numberWithUnsignedInt:kCVPixelFormatType_32RGBA]
+        };
+        AVAssetTrack *inputTrack = [[inputAsset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0];
+        AVAssetReaderOutput *inputOutput = [AVAssetReaderTrackOutput assetReaderTrackOutputWithTrack:inputTrack
+                                                                                outputSettings:videoSettings];
+        [inputReader addOutput:inputOutput];
+        [inputReader startReading];
+
+        int i = 0;
+
+        while (inputReader.status == AVAssetReaderStatusReading)
+        {
+            CMSampleBufferRef sampleBuffer = [inputOutput copyNextSampleBuffer];
+            if (sampleBuffer)
+            {
+                CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+                CVPixelBufferLockBaseAddress(imageBuffer, 0);
+
+                i++;
+                NSLog(@"Got frame %d", i);
+
+                CVPixelBufferUnlockBaseAddress(imageBuffer, 0);
+                CFRelease(sampleBuffer);
+            }
+        }
         
     }
     return 0;
