a08f6ece356726e937d9e296ca77e1779860bde6
Ryan <rsul.dev@me.com>
2014-04-07 15:49:54 -0400

    Add new empty project for refactor

---
+++ interframe/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  interframe
+//
+//  Created by Ryan Sullivan on 4/7/14.
+//  Copyright (c) 2014 RSully. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
